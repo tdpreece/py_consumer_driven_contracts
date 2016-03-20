25613ff94ec2920c33cb8bc6998819f8d0646f7d
Tim Preece <tdpreece@gmail.com>
2016-03-20 13:57:19 +0000

    Can't use mock to capture stdout here becuase it happens in a different process.

--- test/functional_tests/as_a_consumer.py
+++ test/functional_tests/as_a_consumer.py
@@ -1,20 +1,19 @@
-import mock
-from StringIO import StringIO
 import subprocess
 from unittest import TestCase
 
 
 class MockProviderSmokeTest(TestCase):
-    @mock.patch('sys.stdout', new_callable=StringIO)
-    def test_status_OK_returned_when_mock_up_and_running(self, stdout):
+    def test_status_OK_returned_when_mock_up_and_running(self):
         # start mock server with port and hostname
         # send request to /status
         # Check that response is 200 and contains {'status': 'OK'}
         host = '0.0.0.0'
         port = '1911'
-        subprocess.call(['mockprovider', '-h', host, '-p', port])
+        stdout = subprocess.check_output(
+            ['mockprovider', '-h', host, '-p', port]
+        )
         expected_startup_message = 'Mock provider started on {}:{}'.format(
             host,
             port
         )
-        self.assertEqual(stdout.getvalue(), expected_startup_message)
+        self.assertEqual(stdout.strip(), expected_startup_message)

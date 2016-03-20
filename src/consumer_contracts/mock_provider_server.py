0fa4b8cb7308c1116816a71c91f3cfa164f95e32
Tim Preece <tdpreece@gmail.com>
2016-03-20 18:58:46 +0000

    Start flask in daemon process.

--- src/consumer_contracts/mock_provider_server.py
+++ src/consumer_contracts/mock_provider_server.py
@@ -1,4 +1,10 @@
 import argparse
+import os
+
+import daemon
+from flask import Flask
+
+app = Flask(__name__)
 
 
 def get_commandline_arguments():
@@ -11,8 +17,18 @@ def get_commandline_arguments():
     return vars(parser.parse_args())
 
 
+@app.route('/status')
+def status():
+    return '{"status": "OK"}'
+
+
 def main():
     commandline_args = get_commandline_arguments()
     host = "0.0.0.0"
     port = commandline_args.get('port')
-    print('Mock provider started on {}:{}').format(host, port)
+    pid = os.fork()
+    if pid == 0:
+        with daemon.DaemonContext():
+            app.run(host, port)
+    else:
+        print('Mock provider started on {}:{}').format(host, port)
--- test/functional_tests/as_a_consumer.py
+++ test/functional_tests/as_a_consumer.py
@@ -1,12 +1,11 @@
 import subprocess
 from unittest import TestCase
 
+import requests
+
 
 class MockProviderSmokeTest(TestCase):
     def test_status_OK_returned_when_mock_up_and_running(self):
-        # start mock server with port and hostname
-        # send request to /status
-        # Check that response is 200 and contains {'status': 'OK'}
         host = '0.0.0.0'
         port = '1911'
         stdout = subprocess.check_output(
@@ -17,3 +16,9 @@ class MockProviderSmokeTest(TestCase):
             port
         )
         self.assertEqual(stdout.strip(), expected_startup_message)
+        status_url = 'http://{}:{}/status'.format(host, port)
+        response = requests.get(status_url)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.json(), {"status": "OK"})
+
+        # stop server

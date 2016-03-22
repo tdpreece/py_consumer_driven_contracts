8b0a9ed24bc42bedd94372b76fbc3935b5012815
Tim Preece <tdpreece@gmail.com>
2016-03-22 21:13:05 +0000

    Rearrange test to make more obvious.

--- test/functional_tests/as_a_consumer.py
+++ test/functional_tests/as_a_consumer.py
@@ -4,25 +4,27 @@ from unittest import TestCase
 import requests
 
 
-class MockProviderSmokeTest(TestCase):
-    def test_status_OK_returned_when_mock_up_and_running(self):
+class TestControlOfMockProviderServer(TestCase):
+    def test_server_starts_up_and_stops(self):
         host = '0.0.0.0'
         port = '1911'
-        # start server
-        stdout = subprocess.check_output(
-            ['mockprovider', '-p', port, 'start']
-        )
+        status_url = 'http://{}:{}/status'.format(host, port)
         expected_startup_message = 'Mock provider started on {}:{}'.format(
             host,
             port
         )
+        expected_status_json = {"status": "OK"}
+
+        # start server
+        stdout = subprocess.check_output(
+            ['mockprovider', '-p', port, 'start']
+        )
         self.assertEqual(stdout.strip(), expected_startup_message)
 
         # Check server is running
-        status_url = 'http://{}:{}/status'.format(host, port)
         response = requests.get(status_url)
         self.assertEqual(200, response.status_code)
-        self.assertEqual(response.json(), {"status": "OK"})
+        self.assertEqual(response.json(), expected_status_json)
 
         # stop server
         stdout = subprocess.check_output(

27aaa2be01f670a180fab8025360c5a87299f5de
Tim Preece <tdpreece@gmail.com>
2016-04-10 21:46:54 +0100

    Extract methods for loading config and regestering blueprints.

--- src/consumer_contracts/mockprovider.py
+++ src/consumer_contracts/mockprovider.py
@@ -8,20 +8,28 @@ from .blueprints.status import status_blueprint
 from .blueprints.contracts import contracts_blueprint
 
 
-def trim_py_extension(filename):
-    return re.match('(.*)\.py', filename).groups()[0]
-
-
 def create_app(contracts_path):
     app = Flask(__name__)
+    load_contracts_config(app, contracts_path)
+    register_blueprints(app)
+    return app
+
+
+def load_contracts_config(app, contracts_path):
     contracts_dir = path.dirname(contracts_path)
     contracts_filename = path.basename(contracts_path)
     contracts_module = trim_py_extension(contracts_filename)
     sys.path.append(contracts_dir)
     app.config.from_object(contracts_module)
+
+
+def register_blueprints(app):
     app.register_blueprint(status_blueprint, url_prefix='/status')
     app.register_blueprint(contracts_blueprint, url_prefix='/contracts')
-    return app
+
+
+def trim_py_extension(filename):
+    return re.match('(.*)\.py', filename).groups()[0]
 
 
 if __name__ == '__main__':
